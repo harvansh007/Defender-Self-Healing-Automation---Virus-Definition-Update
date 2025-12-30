e3a64f909014c2eb2d34f55a0f11b97bbccd8d36
Harvansh Singh <21218808+harvansh007@users.noreply.github.com>
2025-12-30 16:10:59 +0530

    Add files via upload

---
+++ Remediation script.ps1
@@ -0,0 +1,25 @@
+# This script will check if virus definition updated under 5 days.
+# If Device is updated within 5 days then it will mark as Complaint
+# Change the 5 number according to your standard.
+# This script should used as a Remediation script.
+
+try {
+$path = $((Get-Wmiobject Win32_Service | where-Object {$_.Name -eq "WinDefend"}).Pathname) -replace "MsMpEng.exe", "" -replace "'", "" 
+& "$Path\MpCmdRun.exe" -SignatureUpdate -mmpc
+
+Start-Sleep 10
+
+ If ((Get-MPComputerStatus).AntispywareSignatureAge -le 5 )
+  {
+ Write-Host "Compliant"
+  }
+  else 
+  {
+ Write-Host "Not Compliant"
+  }
+ }
+
+Catch 
+{
+Write-Host "Not Compiant Failure"
+}
